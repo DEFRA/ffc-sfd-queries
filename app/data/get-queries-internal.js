c0e1349079d77739cfb0abb2150d61c91a5eccc8
Rana Salem <ranatasalem@gmail.com>
2024-07-08 11:19:27 +0100

    wip

---
+++ app/data/get-queries-internal.js
@@ -0,0 +1,30 @@
+const Wreck = require('@hapi/wreck')
+const { serverConfig } = require('../config')
+
+const getQueriesInternal = async () => {
+  try {
+    const query = `query {
+       allCustomerQueryTickets {
+        customerQueryTickets {
+            id
+            timestamp
+            heading
+            body
+      }
+    }
+  }`
+    const { payload } = await Wreck.post(serverConfig.dataHost, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      payload: JSON.stringify({ query }),
+      json: true
+    })
+
+    return payload.data.allCustomerQueryTickets.customerQueryTickets
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+module.exports = { getQueriesInternal }
--- app/data/index.js
+++ app/data/index.js
@@ -2,6 +2,7 @@ const { createResponse } = require('./create-response')
 const { createQuery } = require('./create-query')
 const getOrganisation = require('./get-organisation')
 const { getQueries } = require('./get-queries')
+const { getQueriesInternal } = require('./get-queries-internal')
 const { getTicket } = require('./get-ticket')
 
 module.exports = {
@@ -9,5 +10,6 @@ module.exports = {
   createQuery,
   getOrganisation,
   getQueries,
+  getQueriesInternal,
   getTicket
 }
