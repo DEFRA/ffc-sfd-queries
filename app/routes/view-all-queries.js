c90402a08a73aa78278d5f6a11b0b8a8295c0ee8
Rana Salem <ranatasalem@gmail.com>
2024-07-15 15:44:47 +0100

    Update app config (#8)

--- app/routes/view-all-queries.js
+++ app/routes/view-all-queries.js
@@ -1,15 +1,20 @@
 const { GET } = require('../constants/http-verbs')
 const { SFD_VIEW } = require('ffc-auth/scopes')
-const { getOrganisation } = require('../data')
-const { getQueries } = require('../data')
+const { getOrganisation, getQueries } = require('../data')
 
 module.exports = {
   method: GET,
   path: '/view-all',
-  options: { auth: { strategy: 'jwt', scope: [SFD_VIEW] } },
-  handler: async (request, h) => {
-    const organisation = await getOrganisation(request)
-    const queries = await getQueries(organisation.sbi)
-    return h.view('view-all-queries', { queries, organisation })
+  options: {
+    auth: { strategy: 'jwt', scope: [SFD_VIEW] },
+    handler: async (request, h) => {
+      try {
+        const organisation = await getOrganisation(request)
+        const queries = await getQueries(organisation.sbi)
+        return h.view('view-all-queries', { queries, organisation })
+      } catch (error) {
+        throw new Error(error.message)
+      }
+    }
   }
 }
